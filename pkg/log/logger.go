14e653b1ab38cb09a7db83146e12c380102d2578
Beate Ottenwälder <b.ottenwaelder@gmail.com>
2026-02-28 20:10:58 +0100

    reduced logging

--- pkg/log/logger.go
+++ pkg/log/logger.go
@@ -5,51 +5,30 @@ import "log"
 // Logger defines the interface for logging within the library.
 // Consumers should implement this interface to capture library logs.
 type Logger interface {
-	Debug(args ...interface{})
-	Info(args ...interface{})
-	Warn(args ...interface{})
 	Error(args ...interface{})
-
-	Debugf(format string, args ...interface{})
-	Infof(format string, args ...interface{})
-	Warnf(format string, args ...interface{})
 	Errorf(format string, args ...interface{})
 }
 
 // globalLogger is the instance used by the library. Defaults to no-op.
-var globalLogger Logger = &noopLogger{}
+var globalLogger Logger = &noLog{}
 
-// SetLogger allows the consumer to inject a custom logger.
+// SetLogger allows the consumer to inject a custom logger, e.g., it allows to set loggers like
 func SetLogger(l Logger) {
 	if l != nil {
 		globalLogger = l
 	}
 }
 
-// Loggers for internal use
+// Loggers for internal use - by default compatible with a lot of logging libraries
 
-func Debug(args ...interface{}) { globalLogger.Debug(args...) }
-func Info(args ...interface{})  { globalLogger.Info(args...) }
-func Warn(args ...interface{})  { globalLogger.Warn(args...) }
-func Error(args ...interface{}) { globalLogger.Error(args...) }
-
-func Debugf(format string, args ...interface{}) { globalLogger.Debugf(format, args...) }
-func Infof(format string, args ...interface{})  { globalLogger.Infof(format, args...) }
-func Warnf(format string, args ...interface{})  { globalLogger.Warnf(format, args...) }
+func Error(args ...interface{})                 { globalLogger.Error(args...) }
 func Errorf(format string, args ...interface{}) { globalLogger.Errorf(format, args...) }
 
-// noopLogger is the default logger that discards all messages.
-type noopLogger struct{}
+// noLog is the default logger that discards all messages.
+type noLog struct{}
 
-func (n *noopLogger) Debug(args ...interface{}) {}
-func (n *noopLogger) Info(args ...interface{})  {}
-func (n *noopLogger) Warn(args ...interface{})  {}
-func (n *noopLogger) Error(args ...interface{}) {}
-
-func (n *noopLogger) Debugf(format string, args ...interface{}) {}
-func (n *noopLogger) Infof(format string, args ...interface{})  {}
-func (n *noopLogger) Warnf(format string, args ...interface{})  {}
-func (n *noopLogger) Errorf(format string, args ...interface{}) {}
+func (n *noLog) Error(args ...interface{})                 {}
+func (n *noLog) Errorf(format string, args ...interface{}) {}
 
 // StdLogger is a wrapper for the standard library logger to satisfy the Logger interface.
 type StdLogger struct {
@@ -65,13 +44,7 @@ func NewStdLogger(l *log.Logger) Logger {
 	return &StdLogger{l: l}
 }
 
-func (s *StdLogger) Debug(args ...interface{})                 { s.output("DEBUG", args...) }
-func (s *StdLogger) Info(args ...interface{})                  { s.output("INFO", args...) }
-func (s *StdLogger) Warn(args ...interface{})                  { s.output("WARN", args...) }
 func (s *StdLogger) Error(args ...interface{})                 { s.output("ERROR", args...) }
-func (s *StdLogger) Debugf(format string, args ...interface{}) { s.outputf("DEBUG", format, args...) }
-func (s *StdLogger) Infof(format string, args ...interface{})  { s.outputf("INFO", format, args...) }
-func (s *StdLogger) Warnf(format string, args ...interface{})  { s.outputf("WARN", format, args...) }
 func (s *StdLogger) Errorf(format string, args ...interface{}) { s.outputf("ERROR", format, args...) }
 
 func (s *StdLogger) output(level string, args ...interface{}) {
